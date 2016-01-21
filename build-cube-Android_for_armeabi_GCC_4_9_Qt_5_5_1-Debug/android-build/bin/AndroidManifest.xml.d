9fc87d565a039b82a1ebb52abb04177e79acdda3
Austin Brown <brownaus@onid.oregonstate.edu>
2016-01-21 15:12:34 -0800

    Removed useless debug code

--- build-cube-Android_for_armeabi_GCC_4_9_Qt_5_5_1-Debug/android-build/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/Austin/Desktop/Capstone/VolumeViewerAndroid/VolumeViewerAndroid/build-cube-Android_for_armeabi_GCC_4_9_Qt_5_5_1-Debug/android-build/bin/AndroidManifest.xml : \
-/Users/Austin/Desktop/Capstone/VolumeViewerAndroid/VolumeViewerAndroid/build-cube-Android_for_armeabi_GCC_4_9_Qt_5_5_1-Debug/android-build/AndroidManifest.xml \
--- build-cube-Android_for_armeabi_GCC_4_9_Qt_5_5_1-Debug/android-build/gen/org/kde/necessitas/ministro/IMinistroCallback.java.d
+++
@@ -1,3 +0,0 @@
-/Users/Austin/Desktop/Capstone/VolumeViewerAndroid/VolumeViewerAndroid/build-cube-Android_for_armeabi_GCC_4_9_Qt_5_5_1-Debug/android-build/gen/org/kde/necessitas/ministro/IMinistroCallback.java: \
-  /Users/Austin/Desktop/Capstone/VolumeViewerAndroid/VolumeViewerAndroid/build-cube-Android_for_armeabi_GCC_4_9_Qt_5_5_1-Debug/android-build/src/org/kde/necessitas/ministro/IMinistroCallback.aidl 
-
